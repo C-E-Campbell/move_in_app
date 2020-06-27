99157399bfd5c609d27cbdb5d796b22f5b7cfded
Charles <charles.e.campbell1989@gmail.com>
2020-06-27 19:52:20 -0400

    updates

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 require('dotenv').config({ path: '../.env' });
-const { PORT } = process.env;
+const { PORT, CONN } = process.env;
 app.use(express.json());
+const massive = require('massive');
+
+massive({
+  connectionString: CONN,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}).then((dbInstance) => {
+  app.set('db', dbInstance);
+  console.log('Db Connected');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello');
--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -3,7 +3,14 @@ import './Landing.scss';
 export default function Landing() {
   return (
     <div className="landing">
-      <h1>Landing</h1>
+      <h1>Find your ideal roomates!</h1>
+      <h2>Have you been looking for the right roomate?</h2>
+      <form>
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+      <a href="#">Forgot Login Details</a>
     </div>
   );
 }
