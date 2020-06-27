8d91af196a39175fbcb5cb2255a8c1d6574ece4c
Charles <charles.e.campbell1989@gmail.com>
2020-06-27 18:20:34 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Landing from './Landing/Landing.jsx';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Landing/Landing.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Landing.scss';
+export default function Landing() {
+  return (
+    <div className="landing">
+      <h1>Landing</h1>
+    </div>
+  );
+}
