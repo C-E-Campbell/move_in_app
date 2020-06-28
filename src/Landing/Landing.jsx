698a9309078ec7a6b3d4994ac6453a18d2d49706
Charles <charles.e.campbell1989@gmail.com>
2020-06-28 16:03:17 -0400

    added logo

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -27,58 +27,60 @@ export default function Landing() {
     }
   };
   return (
-    <div className="landing">
-      <div className="landing_headline">
-        <h1>Find your ideal roomates!</h1>
-        <h2>Have you been looking for the right roomate?</h2>
-      </div>
-      <div>
-        <form className="login_form" onSubmit={(e) => login(e)}>
-          {registerFlag === false ? null : (
+    <div className="landing_container">
+      <div className="landing_logoBox"></div>
+      <div className="landing">
+        <div className="landing_headline">
+          <h1>Find your ideal roomates!</h1>
+          <h2>Have you been looking for the right roomate?</h2>
+        </div>
+        <div>
+          <form className="login_form" onSubmit={(e) => login(e)}>
+            {registerFlag === false ? null : (
+              <input
+                onChange={(e) => SetName(e.target.value)}
+                type="text"
+                placeholder="Name"
+                value={name}
+                className="login_input"
+              />
+            )}
             <input
-              onChange={(e) => SetName(e.target.value)}
+              onChange={(e) => SetEmail(e.target.value)}
               type="text"
-              placeholder="Name"
-              value={name}
+              placeholder="Email"
+              value={email}
               className="login_input"
             />
-          )}
-          <input
-            onChange={(e) => SetEmail(e.target.value)}
-            type="text"
-            placeholder="Email"
-            value={email}
-            className="login_input"
-          />
-          <input
-            onChange={(e) => SetPass(e.target.value)}
-            type="password"
-            placeholder="Password"
-            value={pass}
-            className="login_input"
-          />
-          {registerFlag === false ? null : (
             <input
-              onChange={(e) => SetConfirmPass(e.target.value)}
+              onChange={(e) => SetPass(e.target.value)}
               type="password"
-              placeholder="Confirm Password"
-              value={confirmPass}
+              placeholder="Password"
+              value={pass}
               className="login_input"
             />
-          )}
-          <button className="login_btn" type="submit">
-            Login
-          </button>
-        </form>
+            {registerFlag === false ? null : (
+              <input
+                onChange={(e) => SetConfirmPass(e.target.value)}
+                type="password"
+                placeholder="Confirm Password"
+                value={confirmPass}
+                className="login_input"
+              />
+            )}
+            <button className="login_btn" type="submit">
+              Login
+            </button>
+          </form>
 
-        <div className="forgot_login">
-          <button onClick={() => SetRegisterFlag(!registerFlag)}>
-            Register
-          </button>
-          <button>Need Help?</button>
+          <div className="forgot_login">
+            <button onClick={() => SetRegisterFlag(!registerFlag)}>
+              Register
+            </button>
+            <button>Need Help?</button>
+          </div>
         </div>
       </div>
-      <div className="landing_logoBox"></div>
     </div>
   );
 }
